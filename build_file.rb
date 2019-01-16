72814553de070875902ee359c198fd50f72b52b0
ced <zuger.cedric@gmail.com>
2019-01-16 14:34:35 +0100

    Basics are working.

--- build_file.rb
+++ build_file.rb
@@ -1,5 +1,6 @@
 require 'pp'
 require 'date'
+require 'odf-report'
 
 class BuildFile
 
@@ -8,12 +9,41 @@ class BuildFile
     read_in_charge
   end
 
+  def go
+    report = ODFReport::Report.new('Courier model poubelles.odt') do |r|
+      dw = dates_for_current_week
+
+      r.add_field :date_jaune_out, dw.shift
+      r.add_field :date_jaune_in, dw.shift
+      r.add_field :date_bleue_out, dw.shift
+      r.add_field :date_bleue_in, dw.shift
+
+      r.add_field :resp_semaine_1, @in_charge[0]
+      r.add_field :resp_semaine_2, @in_charge[1]
+      r.add_field :resp_semaine_3, @in_charge[2]
+    end
+    report.generate('file.odt')
+  end
+
   private
+
+  def dates_for_current_week
+    out_j = format_date(@current_dates[:jaune] - (1/24.0)*6)
+    in_j = format_date(@current_dates[:jaune] + (1/24.0)*10)
+    out_b = format_date(@current_dates[:bleue] - (1/24.0)*6)
+    in_b = format_date(@current_dates[:bleue] + (1/24.0)*10)
+    [out_j, in_j, out_b, in_b]
+  end
+
+  def format_date(date)
+    date.strftime('%A, %-d %B à partir de %H heure')
+  end
 
   def read_dates
     @dates = File.open('dates.txt','r').readlines
     @dates.shift
     @dates = @dates.map{ |e| read_date_line( e ) }
+    @current_dates = @dates.first
   end
 
   def read_date_line(date_line)
@@ -32,4 +62,4 @@ class BuildFile
 
 end
 
-pp BuildFile.new
+pp BuildFile.new.go
