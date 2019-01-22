695364af1a928ca178c79bd693a0b26482969edf
ced <zuger.cedric@gmail.com>
2019-01-22 17:50:38 +0100

    New dates handling.

--- lib/dates_handler.rb
+++ lib/dates_handler.rb
@@ -1,3 +1,5 @@
+require 'active_support/all'
+
 class DatesHandler
 
   attr_reader :current_week_number, :next_week_number, :last_week_number, :out_j, :out_b, :in_j, :in_b
@@ -5,7 +7,8 @@ class DatesHandler
   def read
     @dates = File.open('data/dates.txt','r').readlines
     @dates.shift
-    @dates = @dates.map{ |e| read_date_line( e ) }
+    @dates = @dates.map{ |e| read_date_line( e ) }.flatten
+    # pp @dates
     set_current_date
     self
   end
@@ -13,20 +16,21 @@ class DatesHandler
   private
 
   def set_current_date
-    while(@dates.first[:jaune] < DateTime.now)
+    while((@dates.first[:type] == :bleue && @dates.first[:date] <= DateTime.now) || @dates.first[:type] == :jaune)
       @dates.shift
     end
 
-    @current_dates = @dates.first
+    @blue_date = @dates.shift
+    @yellow_date = @dates.first
 
-    @current_week_number = @current_dates[:jaune].cweek
-    @next_week_number = @current_dates[:jaune].next_day(7).cweek
-    @last_week_number = @current_dates[:jaune].next_day(14).cweek
+    @current_week_number = DateTime.now.cweek
+    @next_week_number = DateTime.now.next_day(7).cweek
+    @last_week_number = DateTime.now.next_day(14).cweek
 
-    @out_j = format_date(@current_dates[:jaune] - (1/24.0)*6)
-    @in_j = format_date(@current_dates[:jaune] + (1/24.0)*10)
-    @out_b = format_date(@current_dates[:bleue] - (1/24.0)*6)
-    @in_b = format_date(@current_dates[:bleue] + (1/24.0)*10)
+    @out_j = format_date(@yellow_date[:date] - (1/24.0)*6)
+    @in_j = format_date(@yellow_date[:date] + (1/24.0)*10)
+    @out_b = format_date(@blue_date[:date] - (1/24.0)*6)
+    @in_b = format_date(@blue_date[:date] + (1/24.0)*10)
   end
 
   def format_date(date)
@@ -36,7 +40,7 @@ class DatesHandler
 
   def read_date_line(date_line)
     dates_data = date_line.strip.split.map{ |e| build_date(e) }
-    { jaune: dates_data[0], bleue: dates_data[1] }
+    [ {type: :jaune, date: dates_data[0], day_before: dates_data[0] - (1/24.0)*6}, {type: :bleue, date: dates_data[1]} ]
   end
 
   def build_date(date_string)
